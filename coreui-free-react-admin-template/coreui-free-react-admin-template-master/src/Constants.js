e8bbdc40a6894bf70e40e024e800d9b6b5ca8359
ttphu2 <ttphu34@gmail.com>
2020-11-25 23:47:49 +0700

    Tao project diagram

--- coreui-free-react-admin-template/coreui-free-react-admin-template-master/src/Constants.js
+++
@@ -1,2 +0,0 @@
-export const API_URL='http://localhost:8081'
-export const JPA_API_URL='http://localhost:8081/jpa'
--- newswebsite/src/main/java/com/javaweb/newswebsite/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.javaweb.newswebsite.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
